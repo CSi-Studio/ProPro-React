bc1d0b474e9f6d47ce028370d12f879e7b9280b7
wanglihuya <wanglihuya@outlook.com>
2021-08-07 17:55:54 +0800

    尝试图标优化

--- src/pages/Library/components/DetailForm.tsx
+++ src/pages/Library/components/DetailForm.tsx
@@ -5,6 +5,7 @@ import type { TableListItem } from '@/pages/Project/data';
 import type { ProDescriptionsItemProps } from '@ant-design/pro-descriptions';
 import ReactECharts from 'echarts-for-react';
 import ProDescriptions from '@ant-design/pro-descriptions';
+import { type } from '../../Project/components/DeleteForm';
 
 export type UpdateFormProps = {
   showDetail: any;
@@ -15,9 +16,11 @@ export type UpdateFormProps = {
 };
 
 const DetailForm: React.FC<UpdateFormProps> = (props) => {
-  // const xAxisData = [];
-  // const data1 = [];
-  // const data2 = [];
+  const xAxisData1 = props?.currentRow?.statistic?.Peptide_Dist_On_Mz_5?.x;
+  const xAxisData2 = props?.currentRow?.statistic?.Peptide_Dist_On_RT_5?.x;
+  const data1 = props?.currentRow?.statistic?.Peptide_Dist_On_Mz_5?.y;
+  const data2 = props?.currentRow?.statistic?.Peptide_Dist_On_RT_5?.y;
+
   // // eslint-disable-next-line no-plusplus
   // for (let i = 0; i < 100; i++) {
   //   xAxisData.push(`类目${i}`);
@@ -25,26 +28,57 @@ const DetailForm: React.FC<UpdateFormProps> = (props) => {
   //   data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
   // }
 
-  const option1 = {
-    xAxis: [
-      {
-        type: 'value',
-        boundaryGap: false,
-        data: props?.currentRow?.statistic?.Peptide_Dist_On_RT_5?.x,
-      },
-    ],
-    yAxis: [
-      {
-        type: 'value',
-      },
-    ],
+  const option = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+      source: [],
+    },
+
+    title: {
+      text: 'Peptide_Dist_On_Mz_5',
+      subtext: 'Peptide_Dist_On_Mz_5',
+    },
+    xAxis: {
+      type: 'category',
+      data: xAxisData1,
+    },
+    yAxis: {
+      type: 'value',
+    },
     series: [
       {
         type: 'bar',
-        data: props?.currentRow?.statistic?.Peptide_Dist_On_RT_5?.y,
+        data: data1,
       },
     ],
   };
+  const option1 = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+      source: [],
+    },
+
+    title: {
+      text: 'Peptide_Dist_On_RT_5',
+      subtext: 'Peptide_Dist_On_RT_5',
+    },
+    xAxis: {
+      type: 'category',
+      data: xAxisData2,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        type: 'bar',
+        data: data2,
+      },
+    ],
+  };
+
   return (
     <Drawer width={800} visible={props.showDetail} onClose={props.onClose} closable={false}>
       {props.currentRow?.name && (
@@ -60,6 +94,7 @@ const DetailForm: React.FC<UpdateFormProps> = (props) => {
           columns={props.columns as ProDescriptionsItemProps<TableListItem>[]}
         />
       )}
+      <ReactECharts option={option} style={{ height: 400 }} />
       <ReactECharts option={option1} style={{ height: 400 }} />
     </Drawer>
   );
