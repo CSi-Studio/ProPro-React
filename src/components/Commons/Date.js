8b2709336738335f3593083fb181d8db074dc173
lms19880914 <lms19880914@qq.com>
2021-08-18 13:37:33 +0800

    new commit

--- src/components/Commons/Date.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-export const format = datatime => {
-  return moment(datatime).format('YYYY-MM-DD HH:mm:ss')
-}
-
-export const formatNoYear = datatime => {
-  return moment(datatime).format('MM-DD HH:mm:ss')
-}
