a0f42e60b68c8cf48f3dfcde1f5f1297dab2418f
wanglihuya <wanglihuya@outlook.com>
2021-08-23 08:47:30 +0800

    添加页脚

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,7 @@
-// import { DefaultFooter } from '@ant-design/pro-layout';
-
 export default () => {
   const defaultMessage = 'ProPro Analyze Platform Developed by CSi Studio';
-
-  // return <DefaultFooter copyright={`2021 ${defaultMessage}`} />;
   return (
-    <div style={{ textAlign: 'center', color: '#666', width: '100%', margin: '6px 0' }}>
+    <div style={{ textAlign: 'center', color: '#888888', width: '100%', margin: '6px 0' }}>
       2021 © {defaultMessage}
     </div>
   );
