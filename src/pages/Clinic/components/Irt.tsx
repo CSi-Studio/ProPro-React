25fca80ce1f8fc47e8bb4acf8a60e8f71be42922
JW <wanglihuaya@outlook.com>
2021-09-17 17:08:51 +0800

    诊所 tabs 优化网络加载

--- src/pages/Clinic/components/Irt.tsx
+++ src/pages/Clinic/components/Irt.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ReactECharts from 'echarts-for-react';
 import ProCard from '@ant-design/pro-card';
-import { irtList } from '../../Irt/service';
 import { IrtOption } from '../../Irt/charts';
 
 // 每行grid的个数
@@ -24,20 +23,8 @@ const IrtCharts: React.FC<IrtChartsProps> = (props: any) => {
   const [handleOption, setHandleOption] = useState({});
   useEffect(() => {
     const op = async () => {
-      const result = await irtList(props.values.selectedExpIds);
-      result.data.forEach((value: { id: any; alias: any }) => {
-        props.values.exps.forEach((item: { id: any; alias: any }) => {
-          if (item.id === value.id) {
-            value.alias = item.alias;
-          }
-        });
-      });
-      result.data.sort((a: { alias: string }, b: { alias: string }) =>
-        a.alias > b.alias ? 1 : -1,
-      );
-
       const irt = new IrtOption(
-        result.data,
+        props.values,
         gridNumberInRow,
         xName,
         yName,
@@ -45,16 +32,14 @@ const IrtCharts: React.FC<IrtChartsProps> = (props: any) => {
         gridPaddingHeight,
       );
       const option = irt.getIrtOption();
-      Height = Math.ceil(result.data.length / gridNumberInRow) * (gridHeight + gridPaddingHeight);
+      Height = Math.ceil(props.values.length / gridNumberInRow) * (gridHeight + gridPaddingHeight);
       setHandleOption(option);
     };
     op();
   }, []);
 
   return (
-    <ProCard
-    // title: <>{props.values.length}个实验的IRT结果</>,
-    >
+    <ProCard>
       <ReactECharts
         option={handleOption}
         style={{ width: `100%`, height: Height }}
