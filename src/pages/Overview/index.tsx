dec2f802eff4333ccedc14bcee39332bd1c28152
lms19880914 <lms19880914@qq.com>
2021-11-09 23:16:09 +0800

    修复ok fine的bug

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -823,7 +823,7 @@ const TableList: React.FC = (props: any) => {
           formDelete?.resetFields();
         }}
         onSubmit={async (value) => {
-          if (value.name === 'of fine') {
+          if (value.name === 'ok fine') {
             const success = await handleRemove(selectedRows);
             if (success) {
               handleDeleteModalVisible(false);
