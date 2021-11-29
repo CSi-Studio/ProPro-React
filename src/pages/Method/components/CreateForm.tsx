3251bfb81bdfa93362fa1f0745a2bdffb21a1600
lms19880914 <lms19880914@qq.com>
2021-11-29 14:59:41 +0800

    增加新的Method参数

--- src/pages/Method/components/CreateForm.tsx
+++ src/pages/Method/components/CreateForm.tsx
@@ -114,6 +114,18 @@ const CreateForm: React.FC<CreateFormProps> = (props) => {
             label="平滑点数"
             placeholder="smoothPoints"
           />
+          <ProFormDigit
+            initialValue={50}
+            name="ionsLow"
+            label="离子碎片低信号阈值"
+            placeholder="ionsLow"
+          />
+          <ProFormDigit
+            initialValue={300}
+            name="ionsHigh"
+            label="离子碎片高信号阈值"
+            placeholder="ionsHigh"
+          />
           <ProFormSelect
             initialValue="PROPRO"
             name="peakFindingMethod"
